3c4f81bb4fb45f43c4b4302916cfb5737ac0fee6
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-01-20 00:52:36 -0600

    redux hooks added to product list

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -3,16 +3,21 @@ import { useQuery } from '@apollo/client';
 import { idbPromise } from '../../utils/helpers';
 
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
+// import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+// redux hooks
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
 
-  const { currentCategory } = state;
+  const { currentCategory, products } = useSelector( state => state.shop);
 
+  const dispatch = useDispatch();
+  
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
@@ -43,10 +48,10 @@ function ProductList() {
 
   function filterProducts() {
     if (!currentCategory) {
-      return state.products;
+      return products;
     }
 
-    return state.products.filter(
+    return products.filter(
       (product) => product.category._id === currentCategory
     );
   }
@@ -54,7 +59,7 @@ function ProductList() {
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
+      {products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
